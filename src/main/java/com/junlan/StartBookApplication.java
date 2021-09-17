0b2bf7bb41274cf1eff89abf8f4a062f0d24be5d
jun lan <jun.lan@moneris.com>
2021-09-17 16:50:32 -0700

    Update code

--- src/main/java/com/junlan/StartBookApplication.java
+++ src/main/java/com/junlan/StartBookApplication.java
@@ -10,17 +10,17 @@ import java.math.BigDecimal;
 @SpringBootApplication
 public class StartBookApplication {
 
-    // start everything
     public static void main(String[] args) {
+
         SpringApplication.run(StartBookApplication.class, args);
     }
 
     @Bean
     CommandLineRunner initDatabase(BookRepository repository) {
         return args -> {
-            repository.save(new Book("A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41")));
-            repository.save(new Book("The Life-Changing Magic of Tidying Up", "Marie Kondo", new BigDecimal("9.69")));
-            repository.save(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", new BigDecimal("47.99")));
+            repository.save(new Book("Book1", "Author1", new BigDecimal("10.00")));
+            repository.save(new Book("Book2", "Author2", new BigDecimal("11.00")));
+            repository.save(new Book("Book3", "Author3", new BigDecimal("12.00")));
         };
     }
 }
